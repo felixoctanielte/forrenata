454c1b8c50c184fad32b905d12892feb1f9e86e0
Felix Octaniel <felixoctanieltel05@gmail.com>
2025-10-29 11:33:32 +0700

    pratice2

---
+++ Learn Gollang/week1/main.go
@@ -0,0 +1,17 @@
+package main
+
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, world!")
+
+	var myName string = "Felix"
+	const mySescondName string = "Octaniel"
+	myThirdName := "dlw"
+
+	fmt.Println("Felix Ganteng ", myName, mySescondName, myThirdName)
+	// fmt.Println(myName)
+	// fmt.Println(mySescondName)
+	// fmt.Println(myThirdName)
+}
